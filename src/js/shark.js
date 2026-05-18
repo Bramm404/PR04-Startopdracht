6d54b8fcaeaa853ecfaac7a0a1225f3acd203969
Bram den Boer <b.h.r.denboer@student.utwente.nl>
2026-05-18 11:39:05 +0200

    vo

--- src/js/shark.js
+++ src/js/shark.js
@@ -2,6 +2,7 @@ import {Resources, ResourceLoader } from "./resources.js";
 import {Actor, randomInRange, Vector} from 'excalibur'
 
 export class Shark extends Actor {
+    speed = 100;
     constructor() {
         super();
         console.log('shork shork');
@@ -17,14 +18,18 @@ export class Shark extends Actor {
     sharkLeft(e) {
         if(e.target.vel.x > 0) {
             e.target.pos = new Vector(1480, randomInRange(0,720))
-            e.target.vel = new Vector(randomInRange(-500, -150), randomInRange(-10,10))
+            e.target.vel = new Vector(-this.speed, randomInRange(-10,10))
             e.target.graphics.flipHorizontal = true;
+            this.speed = this.speed+10;
+            console.log(this.speed)
         }
 
         else {
         this.pos = new Vector(-200,randomInRange(0,720))
-        this.vel = new Vector(randomInRange(150,500), randomInRange(-10,10))
+        this.vel = new Vector(this.speed, randomInRange(-10,10))
         e.target.graphics.flipHorizontal = false;
+        this.speed = this.speed+10;
+        console.log(this.speed)
         }
     }
 
